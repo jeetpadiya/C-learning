bdb11d333e4ee6dc249b1a7b4020014fba602c29
jeetpadiya <gamerintrovert5@gmail.com>
2026-08-20 00:11:01 +0530

    Add basic arithmetic operations in operators.cpp
    - Implemented a simple program to perform addition, subtraction, multiplication, and division on two user-input numbers.
    - Included prompts for user input and displayed results for each operation.

---
+++ inot.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cout << "Enter the value of num1:\n";
+    cin >>num1;  /*>> extraction operator*/
+    cout << "Enter the value of num2:\n";
+    cin >>num2;  /*>> extraction operator*/
+    cout<<"the sum of num1 and num2 is:"<<num1+num2<<endl;
+    return 0;
+}
---
+++ operators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //arithmatic operators
+    double num1,num2;
+    cout << "Entert the value of num1:\n";
+    cin >> num1;
+    cout << "Enter the value of num2:\n";
+    cin >> num2;
+    cout<<"num1+num2:\n"<<num1+num2;
+    cout<<"\n";
+    cout<<"num1*num2:\n"<<num1*num2;
+    cout<<"\n";
+    cout<<"num1-num2:\n"<<num1-num2;
+    cout<<"\n";
+    cout<<"num1/num2:\n"<<num1/num2;
+    return 0;
+}
