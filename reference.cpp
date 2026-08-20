c462c65487813ff98402563628a0abcb85b08a22
jeetpadiya <gamerintrovert5@gmail.com>
2026-08-21 00:48:03 +0530

    Add reference.cpp with examples of variable declaration, type casting, and reference variables
    - Introduced a new file `reference.cpp` demonstrating basic C++ concepts.
    - Included commented-out sections for user input, variable size checks, and reference variables.
    - Added examples of type casting from float to int and displaying values.

---
+++ reference.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int c = 45;
+
+int main()
+{
+    // int a,b,c;
+    // cout<<"Enter the value of A :"<<endl;
+    // cin>>a;
+    // cout<<"Enter the value of B :"<<endl;
+    // cin>>b;
+    // c=a+b;
+    // cout<<"value of sum is :"<<c<<endl;
+    // cout<<"value of sum is :"<<::c<<endl;
+
+    // float d=34.5;
+    // long double e=34.5;
+    // cout<<"The size of 34.5 is  "<<sizeof(d)<<endl;
+    // cout<<"The size of 34.5 is  "<<sizeof(e)<<endl;
+    // cout<<"The size of 34.5 is  "<<sizeof(34.5F)<<endl;
+    // cout<<"The size of 34.5 is  "<<sizeof(34.5l)<<endl;
+    // cout<<"The size of 34.5 is  "<<sizeof(34.5)<<endl;
+
+    // reference variable
+    // float a = 455;
+    // float &b = a;
+    // cout << b << endl;
+
+    // type casting
+    int a = 20.31313;
+    cout << "this is  value of a is : " << a << endl;
+    cout << "this is value of a with typecasting is :" << (double)a << endl;
+    return 0;
+}
